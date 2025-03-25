ff0d1b87f5d9823c13dc0e5db36c5630d9e90bd6
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-25 14:53:13 -0400

    Create Primitives - Numbers.py

---
+++ Primitives - Numbers.py
@@ -0,0 +1,26 @@
+# Numbers
+# In python there are 3 types of numbers
+# 145 - Integer
+# 1.45 - Float
+# a + bi - Complex numbers, where i is the imaginary number
+
+x = 12
+x = 1.45
+x = 1 + 2j # instead of i we use j
+
+# Arithmetic Operators
+print(20 + 3)
+print(20 - 3)
+print(20 * 3)
+print(20 / 3) # we get the output with floating point number
+print(20 // 3) # we get the output with an integer
+print(20 % 3) # modulus - the remainder of the division
+print(20 ** 3) # exponent - power
+
+# We also have Augmented Assignment Operator
+
+x = 10
+x = x + 3
+
+# or use augmented assignment operator
+x += 3

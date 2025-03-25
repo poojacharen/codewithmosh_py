f99b2cd3dde7f0a24aecd5b42302aba250f69ea0
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-25 12:44:37 -0400

    Create Getting started with py.py

---
+++ Getting started with py.py
@@ -0,0 +1,9 @@
+# Quiz
+# 1. What is an expression ?
+#    An expression is a piece of code that produces value
+#    eg: "*" * 3
+# 2. What is a Syntax Error ?
+#    It is a kind of error due to a bad syntax or bad grammar in a code
+# 3. What does a linter do ?
+#    Linter is a tool that checks our code for potential errors. Mostly in a category of syntax
+#    error.

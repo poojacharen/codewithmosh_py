0d2dc97372fd74416b5854e9fc48c4c39a933750
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-25 15:05:27 -0400

    Create Primitives - Working with Numbers.py

---
+++ Primitives - Working with Numbers.py
@@ -0,0 +1,21 @@
+# Working with Numbers
+
+# round(2.6) # rounding a number
+
+print(round(2.6)) # output is 3
+
+# Another built-in function called
+abs(-2.6) # which gives the absolute value
+print(abs(-2.6)) # output is 2.6 (gives a positive value)
+
+# If you want to write a program that involves complex mathematical calculations, we need to use math module
+# A module is like a separate file with some python code.
+# Math module in pyhon includes lot of mathematical functions for working with numbers
+# But we need to import this module, so we can use it
+
+import math  # math in this program is an obj, so we can use a .notation to see the functions more accurately and all the objs available in this obj
+
+math.ceil(2.2) # to get a ceiling of a number
+print(math.ceil(2.2)) # output : 3
+
+# Refer google for more builtin function in math module 

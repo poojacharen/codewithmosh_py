4cdb940681420aac3c8d8a229d9e97b7611b1799
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-25 13:52:54 -0400

    Create primitive types - Variable names.py

---
+++ primitive types - Variable names.py
@@ -0,0 +1,6 @@
+# Variable Names : Should be descriptive and meaningful, so that it makes the code maintainable
+
+student_count = 1000
+rating = 1.09
+course_name = "Python Programming"
+it_is_published = True

7bcbfd0bae48a779224fb18068522667b1ca2b39
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-25 16:35:22 -0400

    Create Primitives - Quiz.py

---
+++ Primitives - Quiz.py
@@ -0,0 +1,14 @@
+# Quiz
+# 1. What are the built-in primitive types in python?
+#    strings, numbers and boolean
+#    numbers can be integers, float, or complex numbers
+# 2. fruit = "Apple"
+#    print(fruit[1] What would be the output of the fruit with index 1?
+#  output is : p
+# 3. fruit = "Apple"
+#     print(fruit[1:-1]
+#  output is : ppl
+# 4. What is the result of this expression 10 % 3
+#    output is : 1
+# 5. What would be the result when we print(bool("False"))
+#    output is : True

31c2eb923e3cc5fd1e05b441d92fdf8f54fc6efa
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-26 14:09:15 -0400

    Create Control Flow - Chaining Comparison Operators.py

---
+++ Control Flow - Chaining Comparison Operators.py
@@ -0,0 +1,10 @@
+# Chaining Comparison Operators
+# Eg: Let's say if we implement a rule saying
+#     age should be between 18 and 60
+
+age = 22
+# if age >= 18 and age < 60:
+#     print("Eligible")
+
+if 18 <= age < 60:
+    print("Eligible")

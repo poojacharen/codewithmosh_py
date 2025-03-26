3625aaee338aebdcfcdfc6b6ad6eb906ca9e9633
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-26 14:15:25 -0400

    Create Control Flow - Quiz.py

---
+++ Control Flow - Quiz.py
@@ -0,0 +1,10 @@
+# Quiz
+
+if 10 == "10":
+    print("a")
+elif "bag" > "apple" and "bag" > "cat":
+    print("b")
+else:
+    print("c")
+# What would be the output of the above code when we run?
+# Ans : c

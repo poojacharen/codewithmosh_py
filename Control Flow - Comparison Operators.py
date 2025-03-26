f81796e83bbf98c1ff21fd201ee9c315e50d2c0d
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-26 13:17:59 -0400

    Create Control Flow - Comparison Operators.py

---
+++ Control Flow - Comparison Operators.py
@@ -0,0 +1,12 @@
+# Comparison Operators : It is used to compare values
+# eg:
+# 10 > 4  True
+# 10 >= 4 True
+# 10 < 40  True
+# 10 <= 40  True
+# 10 == 10  True
+# 10 == "10" False , because this value has different types and it is stored diff in computer memory
+# 10 != 10  True
+# "bag" > "apple"  True
+# "bag" == "BAG"  False
+

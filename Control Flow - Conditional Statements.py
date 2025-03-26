ae190b72e6b74ab6bc46492bbd894cafee233a02
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-26 13:22:59 -0400

    Create Control Flow - Conditional Statements.py

---
+++ Control Flow - Conditional Statements.py
@@ -0,0 +1,11 @@
+# Conditional Statements
+
+temperature = 17
+if temperature > 30:
+    print("It's Warm")
+    print("Drink water")
+elif temperature > 20:
+    print("It's good")
+else:
+    print("It is cold")
+print("Done")

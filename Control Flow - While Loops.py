ebce7897f61266b3f6a7b17fbfefd70284c42210
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-26 15:09:31 -0400

    Create Control Flow - While Loops.py

---
+++ Control Flow - While Loops.py
@@ -0,0 +1,13 @@
+While Loops : It is used to repeat something as long as a condition is true
+# eg:
+number = 100
+while number > 0:
+    print(number)
+    number //= 2
+
+command = ""
+while command != "quit":
+    command = input(">")
+    print("ECHO", command)
+
+# It doesn't quit until we type quit in the terminal   

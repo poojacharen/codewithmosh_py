0dfd146bcd918180159b8b0179bcf47bbe43db55
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-03-27 14:20:43 -0400

    Functions - Arguments

---
+++ Functions - Arguments.py
@@ -0,0 +1,11 @@
+# Arguments
+
+# Parameters : They are the input that is defined for the function
+# Argument : This is the actual value for a given parameter
+
+def greet(first_name, last_name):      # greet(): inside the brackets if mentioned anything are called parameters
+    print(f"Hi {first_name} {last_name} ")
+    print("Welcome aboard")
+
+greet("Poojitha", "Nagallapati")
+greet("Charen", "Koneti")# here inside the brackets they are called as arguments

0084d6b74c18f3246fccc67a84c58dadb7878428
Poojitha <88705030+poojacharen@users.noreply.github.com>
2025-04-09 15:52:38 -0400

    Exceptions - Exception

--- Exceptions - Exception.py
+++ Exceptions - Exception.py
@@ -1,7 +1,9 @@
 # Exceptions : An exception is a kind of error that terminates the execution of a program.
-# When writing programs, many things can go wrong. Our programs may encounter an error and terminate. Usually these errors happen because of programmers mistake, or bad data that we get from the user.
+# When writing programs, many things can go wrong. Our programs may encounter an error and terminate. Usually these errors happen because of programmers mistake, or bad data that 
+# we get from the user.
 # or resources not being available. For eg, we might need to open a file and if that file doesn't exist our program is going to crash.
-# It's our job as a programmer to prevent our application from crashing in this kind of situations. Instead we want to display a proper error message to the user. Like, this file doesn't exists
+# It's our job as a programmer to prevent our application from crashing in this kind of situations. Instead we want to display a proper error message to the user. Like, this file 
+# doesn't exists
 # some eg's are:
 # Eg : 1
 # numbers = [1, 2]
